0f5693c92fbfaa944488b99679d0703f2060275d
dexfotu <dex.fotu@gmail.com>
2017-01-31 08:32:17 +1300

    Added more animation

--- Assets/Scripts/BunnyController.cs
+++ Assets/Scripts/BunnyController.cs
@@ -1,28 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class BunnyController : MonoBehaviour {
 
 	private Rigidbody2D myRigidbody;
 	private Animator myAnim;
 	public float bunnyJumpForce = 500f;
+	private float bunnyHurtTime = -1;
+	private Collider2D myCollider;
+	public Text scoreText;
+	private float startTime;
 
 	// Use this for initialization
 	void Start () {
 	
 		myRigidbody = GetComponent<Rigidbody2D> ();
 		myAnim = GetComponent<Animator> ();
+		myCollider = GetComponent<Collider2D> ();
+
+		startTime = Time.time;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetButtonUp("Jump")) {
-		
-			myRigidbody.AddForce(transform.up*bunnyJumpForce);
-		}
+		if (bunnyHurtTime == -1) {
 
-		myAnim.SetFloat ("vVelocity",myRigidbody.velocity.y);
+			if (Input.GetButtonUp("Fire1")||Input.GetButtonUp ("Jump")) {
+		
+				myRigidbody.AddForce (transform.up * bunnyJumpForce);
+			}
+
+			myAnim.SetFloat ("vVelocity", myRigidbody.velocity.y);
+			scoreText.text = (Time.time - startTime).ToString ("0.0");
+
+		} else {
+			if (Time.time > bunnyHurtTime + 2) {
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+	
+		if(collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy")){
+
+
+			foreach (PrefabSpawner spawner in FindObjectsOfType<PrefabSpawner>()) {
+				spawner.enabled = false;
+			}
+
+			foreach (MoveLeft moveLefter in FindObjectsOfType<MoveLeft>()) {
+				moveLefter.enabled = false;
+			}
+
+			bunnyHurtTime = Time.time;
+			myAnim.SetBool ("bunnyHurt", true);
+			myRigidbody.velocity = Vector2.zero;
+			myRigidbody.AddForce (transform.up * bunnyJumpForce);
+			myCollider.enabled = false;
+		}
+	
 	}
 }
---
+++ Assets/Scripts/DestroyOnHit.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnHit : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter2D (Collider2D other) {
+		Destroy (other.gameObject);
+	}
+}
